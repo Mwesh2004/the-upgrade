f5ea42bf4ee9e469f6fb5b8c8f269e33670ed3c5
Beryl Munyao <167473598+Mwesh2004@users.noreply.github.com>
2026-08-09 23:45:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        portal: resolve(__dirname, 'portal.html'),
-        privacy: resolve(__dirname, 'privacy.html'),
-        terms: resolve(__dirname, 'terms.html'),
-        disclaimer: resolve(__dirname, 'disclaimer.html'),
-        contact: resolve(__dirname, 'contact.html')
+        main: "index.html",
+        portal: "portal.html",
+        privacy: "privacy.html",
+        terms: "terms.html",
+        disclaimer: "disclaimer.html",
+        contact: "contact.html"
       }
     }
   }
